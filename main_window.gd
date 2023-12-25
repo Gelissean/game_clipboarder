2cf2f3a1c20d5f04e22bb201800367d495754224
Gelissean <gelu586@gmail.com>
2023-12-25 12:36:35 +0100

    changed rating logic

--- main_window.gd
+++ main_window.gd
@@ -3,6 +3,7 @@ extends Control
 var wishlist_text = ":foxwow:"
 var demo_text = ":fox+1:"
 var interesting_text = ":foxhmmm:"
+var garbage_text = ":trash:"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,11 +23,27 @@ func _on_copy_to_clipboard_pressed():
 		return
 	var string_lines = []
 	string_lines.append("**"+$Panel/MarginContainer/VBoxContainer/Name/LineEdit.text+"**")
-	if $Panel/MarginContainer/VBoxContainer/Tags/Wishlisted/CheckBox.button_pressed:
-		string_lines.append("Wishlisted: "+ wishlist_text)
-	if $Panel/MarginContainer/VBoxContainer/Tags/DemoAvailable/CheckBox.button_pressed:
+	var new_element = get_rating()
+	if new_element != null:
+		string_lines.append(new_element)
+	if $Panel/MarginContainer/VBoxContainer/DemoAvailable/CheckBox.button_pressed:
 		string_lines.append("Demo Available: "+ demo_text)
-	if $Panel/MarginContainer/VBoxContainer/Tags/Interesting/CheckBox.button_pressed:
-		string_lines.append("Interesting: " + interesting_text)
 	string_lines.append($Panel/MarginContainer/VBoxContainer/Link/LineEdit.text)
 	DisplayServer.clipboard_set('\n'.join(string_lines))
+
+func get_rating():
+	var menu :Label = $Panel/MarginContainer/VBoxContainer/RatingPanel/Label
+	var text = menu.text
+	print(text)
+	var rating = null
+	if text ==  "Garbage":
+		rating = "Rating: " + text + " "+ garbage_text
+	elif text == "Wishlisted":
+		rating =  "Rating: " + text + " "+ wishlist_text
+	elif text == "Interesting":
+		rating =  "Rating: " + text + " "+ interesting_text
+	return rating
+
+func _on_rating_id_pressed(id):
+	var menu = $Panel/MarginContainer/VBoxContainer/RatingPanel/MenuBar/Rating
+	$Panel/MarginContainer/VBoxContainer/RatingPanel/Label.text = menu.get_item_text(id)
